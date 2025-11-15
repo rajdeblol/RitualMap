e3ca57f900e5547517648b011407c9b867419de9
RajLol <138264795+rajdeblol@users.noreply.github.com>
2025-11-15 13:19:33 +0530

    Create join.js

---
+++ api/api/join.js
@@ -0,0 +1,13 @@
+let summonings = []; // Shared memory
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+    data.id = Date.now().toString() + Math.random().toString(36).substr(2, 5);
+    data.timestamp = Date.now();
+    summonings.push(data);
+    res.status(200).json({ success: true });
+  } else {
+    res.status(405).end();
+  }
+}

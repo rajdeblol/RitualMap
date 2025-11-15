9f62654fba8c8dd9aef5d4f1b6eaf3d666545e0c
RajLol <138264795+rajdeblol@users.noreply.github.com>
2025-11-15 13:31:17 +0530

    Update join.js

--- api/api/join.js
+++ api/api/join.js
@@ -1,4 +1,4 @@
-ilet summonings = []; // Shared in-memory
+let summonings = []; // Shared in-memory
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
@@ -6,7 +6,7 @@ export default function handler(req, res) {
     data.id = Date.now().toString() + Math.random().toString(36).substr(2, 5);
     data.timestamp = Date.now();
     summonings.push(data);
-    res.status(200).json({ success: true });
+    res.status(200).json({ success: true, data });
   } else {
     res.status(405).end();
   }

999c7372b548615e725095bad8629697f26f91d1
RajLol <138264795+rajdeblol@users.noreply.github.com>
2025-11-15 13:43:33 +0530

    Update join.js

--- api/api/join.js
+++ api/api/join.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
     data.id = Date.now() + Math.random().toString(36).substr(2, 5);
     data.timestamp = Date.now();
     summonings.push(data);
-    res.status(200).json({ success: true, data });
+    res.status(200).json({ success: true, data }); // ← MUST RETURN `data`
   } else {
     res.status(405).end();
   }
